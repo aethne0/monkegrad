d8674bd63d38d9dc94e7a5c81e87d10bfb94e797
aethne0 <aethnem@gmail.com>
2025-03-16 15:04:47 -0400

    delete testing stuff

--- monkegrad/tensor.py
+++ monkegrad/tensor.py
@@ -3,40 +3,6 @@ from typing import List
 import numpy as np
 
 
-"""
-def case():
-    x, y = uni(0.0, 1.0), uni(0.0, 1.0)
-    x_b, y_b = x > 0.5, y > 0.5
-    return ((x, y), (1.0 if (x_b and not y_b) or (y_b and not x_b) else 0.0,))
-
-
-cases = 10
-train_data = [case() for _ in range(cases)]
-test_data = [case() for _ in range(cases)]
-
-relu = np.vectorize(lambda x: max(0, x))  # relu
-
-w_inner = np.array([[0.638, 0.366], [0.880, 0.878]])
-w_output = np.array([[0.282], [0.763]])
-
-
-for datum in train_data:
-    inputs = np.array(datum[0])
-    # infer
-    output = np.matmul(inputs, w_inner)
-    output = relu(output)
-    output = np.matmul(output, w_output)
-    output = relu(output)
-    quantized_output = 1.0 if output[0] > 0.5 else 0.0
-    # expected is already quantized
-    print(
-        f"{output[0]:.2f}",
-        f"- {quantized_output:.2f}",
-        f" | matches: {abs(quantized_output - datum[1][0]) > 0.01}",
-    )
-"""
-
-
 def relu(x):
     return max(0, x)
 
@@ -64,29 +30,20 @@ class MLP:
         out += f"input (dim: {self._input_dim})\n"
         out += str(self.inputs) + "\n"
         for i, layer in enumerate(self.layers):
-            out += f"layer-weights {i} (dim: {self._layer_dims[i]}):\n"
+            out += f"layer-{i} weights (dim: {self._layer_dims[i]}):\n"
             out += str(layer) + "\n"
         return out
 
     def fw(self):
-        print("forward pass...")
         output = self.inputs
         for layer in self.layers:
-            print("mul:")
-            print(output)
-            print(layer)
-            output = np.matmul(output, layer)
-            print("result")
-            print(output)
-            output = self.act(output)
-            print("act(result)")
-            print(output)
+            output = self.act(np.matmul(output, layer))
 
-        return output
+        # will always be 1d
+        return output[0]
 
 
-xor_nn = MLP(2, [3, 1], tanh)
-# print(xor_nn)
-print("\n" + str(xor_nn) + "\n")
-# xor_nn.inputs = np.array([[0.25, 0.75]])
-xor_nn.fw()
+if __name__ == "__main__":
+    xor_nn = MLP(2, [3, 1], tanh)
+    print("\n" + str(xor_nn) + "\n")
+    print(xor_nn.fw())
