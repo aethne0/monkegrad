6afaf3cc90761ac840b6bf651f23b312bfee2833
aethne0 <nah>
2025-03-16 22:44:34 -0400

    restarting backprop...

--- monkegrad/tensor2.py
+++ monkegrad/tensor2.py
@@ -4,7 +4,7 @@ from math import tanh
 import numpy as np
 
 np.random.seed(sum([ord(ch) for ch in "monke"]))
-_DEBUG = True
+_DEBUG = False
 
 
 class Op_Enum(Enum):
@@ -14,30 +14,48 @@ class Op_Enum(Enum):
 
 class Op:
     def __init__(self):
+        self.args = ()
         self.arity = 0
+        self.out = 0.0
+        self.grad = 0.0
+
+    def __repr__(self):
+        return f"{type(self)}:(out={self.out:.2f}, grad={self.grad:.2f})"
+
+
+def Op_disp(op_or_nd, indent=0):
+    if type(op_or_nd) is not np.ndarray:
+        print(" " * indent * 4 + str(op_or_nd))
+        for i in op_or_nd.args:
+            Op_disp(i, indent + 1)
+    else:
+        print(" " * indent * 4 + "TENSOR:shape=" + str(op_or_nd.shape))
 
 
 class Tanh(Op):
-    def __init__(self):
+    def __init__(self, args):
+        super().__init__()
         self.arity = 1
+        self.args = args
 
-    def fw(self, x):
-        return np.vectorize(tanh)(x)
+    def fw(self):
+        self.out = np.vectorize(tanh)(self.args[0].out)
 
     def bw(self, out):
-        return (np.vectorize(lambda x: 1 - tanh(x) ** 2)(out),)
+        self.grad = (np.vectorize(lambda x: 1 - tanh(x) ** 2)(out),)
 
 
 class Matmul(Op):
     def __init__(self, args):
+        super().__init__()
         self.arity = 2
         self.args = args
 
     def fw(self):
-        return np.matmul(self.args[0], self.args[1])
+        self.out = np.matmul(self.args[0].out, self.args[1].out)
 
     def bw(self):
-        return (self.args[1].transpose(), self.args[0].transpose)
+        self.grad = (self.args[1].transpose(), self.args[0].transpose)
 
 
 ops_fw = {
@@ -46,19 +64,7 @@ ops_fw = {
 }
 
 # do/dA ( A B ) = B.transpose
-
-_INDENT_MOD = 3
-
-
-def expr_str(expr, indent=0):
-    if type(expr) is tuple:
-        out = f"{' ' * indent}{str(expr[0])}"
-        for e in expr[1:]:
-            out += f"\n{expr_str(e, indent + _INDENT_MOD)}"
-    else:
-        out = f"{' ' * indent}TENSOR:{expr.shape}"
-
-    return out
+# (allegedly)
 
 
 def fw_expr(expr):
@@ -70,8 +76,20 @@ def fw_expr(expr):
         else:
             raise RuntimeError("invalid op?")
     else:
-        expr = expr
+        print(expr)
         return expr
+
+
+def expr_str(expr, indent=0):
+    _spacing = 3
+    _in, _in2 = "|" + _spacing * " ", "|" + _spacing * "-"
+    if type(expr) is tuple:
+        out = f"{_in * (indent - 1) + (_in2 if indent > 0 else '')}{str(expr[0])}"
+        for e in expr[1:]:
+            out += f"\n{expr_str(e, indent + 1)}"
+    else:
+        out = f"{_in * (indent - 1) + (_in2 if indent > 0 else '')}TENSOR:shape={expr.shape}"
+    return out
 
 
 def print_stuff(inputs, layers):
@@ -85,7 +103,7 @@ def print_stuff(inputs, layers):
 
 
 if __name__ == "__main__":
-    mlp = (2, (1,))
+    mlp = (2, (2, 1))
     # inputs = np.random.uniform(0, 1, [1, mlp[0]])
     inputs = np.array([[-0.25, 0.75]])
 
@@ -114,3 +132,11 @@ if __name__ == "__main__":
 
         print("\ninferance:")
         print(fw_expr(expr))
+    print(expr_str(expr), "\n")
+
+    # build expr 2
+    expr_obj = inputs
+    for lay in layers:
+        expr_obj = Tanh((Matmul((expr_obj, lay)),))
+
+    Op_disp(expr_obj)
